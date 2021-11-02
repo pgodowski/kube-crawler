a5ca6e8e346cdb99ffffd251b5baaa46445de5c2
pgodowski <Piotr.Godowski@pl.ibm.com>
2021-11-02 22:09:51 +0100

    scan image in the k8s way (via Job)

--- crawler.sh
+++ crawler.sh
@@ -1,12 +1,31 @@
 #!/bin/sh
 
 
-for ns in `kubectl get namespaces --no-headers | awk '{ print $1; }'`; do 
+#for ns in `kubectl get namespaces --no-headers | awk '{ print $1; }'`; do 
+for ns in `echo default`; do
     echo "Namespace: ${ns}, checking Pods"
     for pod in `kubectl get pods -n ${ns} --no-headers | awk '{ print $1; }'`; do
         #echo  "\tPod: ${pod}, namespace: ${ns}"
         for image in `kubectl get pod ${pod} -n ${ns} -o=jsonpath='{.spec.containers[*].image}'`; do
             echo  "\tPod: ${pod}, namespace: ${ns}, image: ${image}"
+
+cat <<EOF | kubectl apply -f -
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: scan-${pod}
+  namespace: ${ns}
+spec:
+  template:
+    spec:
+      containers:
+      - name: image-test
+        image: ${image}
+        command: ["find",  "/", "-name", "*.swidtag"]
+      restartPolicy: Never
+  backoffLimit: 1
+EOF
+# TODO: do something with scan resultss
         done
     done
 done
